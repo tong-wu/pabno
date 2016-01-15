4809b38b5dc12ae19e67039738331f7b9f224630
Tong Wu <tong@99gamers.com>
2016-01-15 15:52:35 -0800

    fix the VoteService to grab the question and user from id when we initialize so we don't need to do it multiple times.

--- app/services/vote_service.rb
+++ app/services/vote_service.rb
@@ -2,14 +2,14 @@ class VoteService
   def initialize(options, selected, question_id, user_id)
     @options = options
     @selected = selected
-    @question_id = question_id
-    @user_id = user_id
+    @question = Question.find(question_id)
+    @user = User.find(user_id)
   end
 
   def vote
     begin
       if check_if_option_valid
-        Question.find(question_id).vote(user_id, selected)
+        @question.vote(user_id, selected)
       else
         false
       end
@@ -25,7 +25,7 @@ class VoteService
 
   def check_if_option_valid
     if options.include? selected
-      User.find(user_id).check_if_can_vote
+      @user.check_if_can_vote
     end
   end
 end
